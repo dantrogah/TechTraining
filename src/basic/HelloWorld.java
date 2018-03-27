67183b6113ef61ebbd3ad2174d9cfd980e8636fb
dantrogah <31760661+dantrogah@users.noreply.github.com>
2018-03-27 07:15:42 +0000

    Update HelloWorld.java

--- src/basic/HelloWorld.java
+++ src/basic/HelloWorld.java
@@ -6,7 +6,7 @@ public class HelloWorld {
 		
 		System.out.println("Hi My name is Daniel \nI am a Software Developer and "
 				+ "a seasoned instructor.");
-
+		System.out.println("My passion is Computer Programming - Software Development and Graphic Design");
 	}
 
 }
