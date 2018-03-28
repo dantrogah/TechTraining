ad8fa502d0fb2774d674719519d1abc40b5f728b
Dan <daniel.ahiatrogah@gmail.com>
2018-03-27 19:07:21 -0800

    add HelloWorld.java file

---
+++ src/basic/HelloWorld.java
@@ -0,0 +1,12 @@
+package basic;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hi My name is Daniel \nI am a Software Developer and "
+				+ "a seasoned instructor.");
+
+	}
+
+}
